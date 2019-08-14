076931acc1f39b1fe4473c09676d975c8da8b642
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-08-14 19:51:10 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const questionElement = document.getElementById("question");
+let correctAnswerNumber = null;
+
+async function fetchQuestion() {
+    r = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+    j = await r.json();
+    d = j["results"][0];
+    questionElement.innerHTML = d["question"];
+    correctAnswerNumber = Math.floor(Math.random() * 4);
+    for(let i = 0; i < 4; i++) {
+        if(i === correctAnswerNumber) {
+            document.getElementById("answer-" + String(i)).innerHTML = d["correct_answer"];
+        }
+        else {
+            document.getElementById("answer-" + String(i)).innerHTML = d["incorrect_answers"].pop();
+        }
+    }
+}
+
+function colorAnswers() {
+    for(let i = 0; i < 4; i++) {
+        if(i === correctAnswerNumber) {
+            document.getElementById("answer-" + String(i)).className += " correct";
+        }
+        else {
+            document.getElementById("answer-" + String(i)).className += " incorrect";
+        }
+    }
+    correctAnswerNumber = null;
+}
+
+function cleanup() {
+    for(let i = 0; i < 4; i++) {
+        let answerElement = document.getElementById("answer-" + String(i));
+        answerElement.className = answerElement.className.replace(" correct", "");
+        answerElement.className = answerElement.className.replace(" incorrect", "");
+    }
+}
+
+document.onkeypress = async function(e) {
+    if(correctAnswerNumber == null) {
+        await fetchQuestion();
+        await cleanup();
+    }
+    else {
+        await colorAnswers();
+    }
+};
